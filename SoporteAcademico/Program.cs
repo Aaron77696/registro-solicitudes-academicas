52dfa9276c12f9f5ee5fffd793584f6e40bc3982
Aaron77696 <aaronblancosanchez2007>
2026-09-21 08:35:28 -0500

    Requerimiento 1: Registrar datos básicos de una solicitud

--- SoporteAcademico/Program.cs
+++ SoporteAcademico/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+    bool ValidarCodigo(string codigo, int longitudMin)
+    {
+        return true;
+    }
+
+    bool ValidarTipoConsulta(string tipoconsulta)
+    {
+        return true;
+    }
+    
+    bool RegistrarDatos(string codigo, string nombre, string tipoconsulta, string descripcionbreve, int longitudMin)
+    {
+        bool codValido = ValidarCodigo(codigo, longitudMin);
+        bool tipoValido = ValidarTipoConsulta(tipoconsulta);
+        return codValido && tipoValido;
+    }
+
+
+    bool esCorrecto = RegistrarDatos("N00101920", "Marta", "constancia", "Descargar constancia de asistencia", 8);
+
+	if (esCorrecto)
+    {
+		Console.WriteLine("Datos ingresados exitosamente.");
+    }
+
