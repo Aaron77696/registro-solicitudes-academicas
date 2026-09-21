d1e00ba9366b5481830fff10f94f13170e52ef22
Aaron77696 <aaronblancosanchez2007>
2026-09-21 12:03:58 -0500

    Requerimiento 2: Validar el código de estudiante.

--- SoporteAcademico/Program.cs
+++ SoporteAcademico/Program.cs
@@ -1,7 +1,34 @@
 ﻿
     bool ValidarCodigo(string codigo, int longitudMin)
     {
-        return true;
+    if (codigo == "")
+    {
+        return false;
+    }
+    if (codigo.Length < longitudMin)
+    {
+        return false;
+    }
+    if (codigo[0] != 'N')
+    {
+        return false;
+    }
+     if (codigo[1] != '0' || codigo[2] != '0')
+    {
+        return false;
+    }
+
+
+    bool restoSonDigitos = true;
+    for (int i = 1; i < codigo.Length; i++)
+    {
+        if (codigo[i] < '0' || codigo[i] > '9')
+        {
+            restoSonDigitos = false;
+        }
+    }
+
+    return restoSonDigitos;
     }
 
     bool ValidarTipoConsulta(string tipoconsulta)
@@ -9,15 +36,15 @@
         return true;
     }
     
-    bool RegistrarDatos(string codigo, string nombre, string tipoconsulta, string descripcionbreve, int longitudMin)
+    bool RegistrarDatos(string codigo, string nombre, string tipoconsulta, string descripcionbreve)
     {
-        bool codValido = ValidarCodigo(codigo, longitudMin);
-        bool tipoValido = ValidarTipoConsulta(tipoconsulta);
-        return codValido && tipoValido;
+        int longitudMin = 8;
+        bool esBoleano1 = (ValidarCodigo(codigo, longitudMin)==true) && (ValidarTipoConsulta(tipoconsulta)==true);
+        return esBoleano1;
     }
 
 
-    bool esCorrecto = RegistrarDatos("N00101920", "Marta", "constancia", "Descargar constancia de asistencia", 8);
+    bool esCorrecto = RegistrarDatos("N00101920", "Marta", "constancia", "Descargar constancia de asistencia");
 
 	if (esCorrecto)
     {
